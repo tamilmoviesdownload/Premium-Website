ba073a19a162951695a85db5ad1fb37e8088030a
tamilmoviesdownload <ajayneymar14@gmail.com>
2025-12-07 14:38:52 +0530

    Create firebase-config.js

---
+++ firebase-config.js/firebase-config.js
@@ -0,0 +1,10 @@
+// 𝐔𝐬𝐞 𝐭𝐡𝐢𝐬 𝐟𝐨𝐫 𝐚𝐥𝐥 𝐟𝐫𝐨𝐧𝐭𝐞𝐧𝐝 𝐩𝐚𝐠𝐞𝐬
+const firebaseConfig = {
+  apiKey: "AIzaSyBBfZ3YxPgzt-ZgMmcNrNa9KgCUKcuoAvw",
+  authDomain: "premium-website-0628.firebaseapp.com",
+  projectId: "premium-website-0628",
+  storageBucket: "premium-website-0628.appspot.com",
+  messagingSenderId: "945918608964",
+  appId: "1:945918608964:web:c4a89c96fb12101083de11",
+  measurementId: "G-R1BSSTENF2"
+};
